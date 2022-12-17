98b0600fa3dd2625bc28fd9017b9c3816ed1dafb
Rahul Rawat <81628422+RRrawat@users.noreply.github.com>
2022-12-18 00:20:37 +0530

    updated backend configuration

--- terrafrom/backendstate.tf
+++ terrafrom/backendstate.tf
@@ -4,8 +4,8 @@
 
 terraform {
   backend "s3" {
-    bucket = "practice-bucket030722"
-    key    = "api-gateway/terraform.tfstate"
+    bucket = "<Bucket Name>"
+    key    = "<S3 bucket_key>/terraform.tfstate"
     region = "ap-south-1"
   }
 }
