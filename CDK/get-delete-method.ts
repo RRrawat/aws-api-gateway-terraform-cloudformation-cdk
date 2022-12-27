a39d73b19b654e155f7c1b43a780ce3bac84ab74
Rahul Rawat <81628422+RRrawat@users.noreply.github.com>
2022-12-27 12:36:30 +0530

    Create get-delete-method.ts

---
+++ CDK/get-delete-method.ts
@@ -0,0 +1,29 @@
+import * as apigateway from 'aws-cdk-lib/aws-apigateway';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
+import * as cdk from 'aws-cdk-lib';
+import * as path from 'path';
+
+
+export class CdkStarterStack extends cdk.Stack {
+  constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
+    super(scope, id, props)
+
+    // ... rest
+
+    // 👇 define GET todos function
+    const getTodosLambda = new lambda.Function(this, 'get-todos-lambda', {
+      runtime: lambda.Runtime.NODEJS_16_X,
+      handler: 'index.main',
+      code: lambda.Code.fromAsset(path.join(__dirname, '/../src/get-todos')),
+    });
+
+    // 👇 add a /todos resource
+    const todos = api.root.addResource('todos');
+
+    // 👇 integrate GET /todos with getTodosLambda
+    todos.addMethod(
+      'GET',
+      new apigateway.LambdaIntegration(getTodosLambda, {proxy: true}),
+    );
+  }
+}
