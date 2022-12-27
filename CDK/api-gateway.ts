da6c8432bf7f005a44c39dca015aa23eb114b747
Rahul Rawat <81628422+RRrawat@users.noreply.github.com>
2022-12-27 12:34:48 +0530

    Create api-gateway.ts

---
+++ CDK/api-gateway.ts
@@ -0,0 +1,30 @@
+import * as apigateway from 'aws-cdk-lib/aws-apigateway';
+import * as cdk from 'aws-cdk-lib';
+
+export class CdkStarterStack extends cdk.Stack {
+  constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    const api = new apigateway.RestApi(this, 'api', {
+      description: 'example api gateway',
+      deployOptions: {
+        stageName: 'dev',
+      },
+      // 👇 enable CORS
+      defaultCorsPreflightOptions: {
+        allowHeaders: [
+          'Content-Type',
+          'X-Amz-Date',
+          'Authorization',
+          'X-Api-Key',
+        ],
+        allowMethods: ['OPTIONS', 'GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
+        allowCredentials: true,
+        allowOrigins: ['http://localhost:3000'],
+      },
+    });
+
+    // 👇 create an Output for the API URL
+    new cdk.CfnOutput(this, 'apiUrl', {value: api.url});
+  }
+}
